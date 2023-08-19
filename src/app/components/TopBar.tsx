9cd09c180812f24f2df514b9e1b06a7e286f27ee
amitoj-saini <97138788+amitoj-saini@users.noreply.github.com>
2023-08-19 01:27:20 -0400

    Added

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -1,8 +1,35 @@
+import "../../styles/topbar.css";
+
 export default function TopBar() {
-    return (
-        <div className="w-full h-16 border-b grid-border grid grid-flow-row auto-rows-max">
-            <div className="h-16 w-20 border-r grid-border flex justify-center items-center">
+    /*return (
+        <div className="w-full h-16 border-b grid-border flex grid-flow-row auto-rows-max">
+            <div className="h-16 w-16 sm:w-20 border-r grid-border flex justify-center items-center">
                 <img src="/disc.png" alt="logo" width={35} height={35} />
+            </div>
+
+            <div className="h-16 flex justify-center items-center">
+                <h1 style={{ backgroundImage: 'var(--gradient-main)' }} className="mx-5 hidden sm:block text-xl text-gradient font-semibold">Disc</h1>
+            </div>
+
+            <div className="h-16 w-20 ml-auto border-l grid-border hidden sm:block"></div>
+        </div>
+    )*/
+    return (
+        <div className="w-full h-16 px-5 sm:px-10 flex auto-cols-max flex items-center">
+            <div className="flex">
+                <img src="/disc.png" alt="logo" width={35} />
+                <h1 style={{ backgroundImage: 'var(--gradient-main)' }} className="text-gradient text-xl font-light ml-2">Disc</h1>
+            </div>
+            <div className="flex justify-center text-center flex-grow ">
+                <div className="center-nav-menu hidden sm:flex">
+                    <div>About Us</div>
+                    <div>How it works</div>
+                    <div>Blog</div>
+                </div>
+            </div>
+            <div className="flex">
+                <button>Get Started</button>
+                <button>Login</button>
             </div>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,43 @@ import "../styles/index.css";
 
 export default function Home() {
     return (
-        <TopBar></TopBar>
+        <main className="main">
+            <TopBar></TopBar>
+            <div className="w-full px-5 sm:px-10 flex">
+                <div className="w-full mt-16 md:mt-25 lg:mt-32 xl:mt-44 flex justify-center">
+                    <div>
+                        <h1 style={{ backgroundImage: 'var(--gradient-second)' }} className="p-1 font-bold text-2xl md:text-3xl max-w-sm lg:text-4xl lg:max-w-xl xl:text-5xl xl:max-w-2xl 2xl:text-8xl 2xl:max-w-5xl text-gradient">The Simpler Way to Create Discord Bots</h1>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
+
+/*
+import TopBar from "./components/TopBar";
+import "../styles/index.css";
+
+export default function Home() {
+    return (
+        <main className="main">
+            <TopBar></TopBar>
+            <div className="bottom-container flex grid-flow-row auto-rows-max">
+                <div className="w-20 border-r grid-border hidden sm:block"></div>
+                <div className="grid-cols flex-grow flex justify-center">
+                    <div className="mt-20 flex grid-cols-3">
+                        <div className="col-span-3">
+                            <h1 style={{ backgroundImage: 'var(--gradient-main)' }} className="p-1 font-bold text-2xl lg:text-4xl xl:text-5xl text-gradient">The Simpler Way to Create Discord Bots</h1>
+                        </div>
+                        <div></div>
+                    </div>
+                </div>
+                <div className="w-20 ml-auto border-l grid-border hidden sm:block"></div>
+            </div>
+        </main>
+    )
+}
+*/
