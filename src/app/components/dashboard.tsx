2f154687894ca9e0442281d906f8e3fc75c2a793
amitoj-saini <97138788+amitoj-saini@users.noreply.github.com>
2023-09-07 00:59:50 -0400

    Adding trending page

--- src/app/components/dashboard.tsx
+++ src/app/components/dashboard.tsx
@@ -1,25 +1,64 @@
+"use client";
+
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ReactNode, useState } from "react";
 import "../../styles/dashboard.css";
 
-export default async function DashboardTemplate() {
+
+
+
+export default function DashboardTemplate({children} : {children: ReactNode}) {
+    const [shrinkMenu, shrinkMenuClicked] = useState(false);
+    
+    if (typeof window !== "undefined") {
+        let shrinkMenuValue = localStorage.getItem("shrinkMenu") ? true : false;
+        console.log(shrinkMenu != shrinkMenuValue)
+        if (shrinkMenu != shrinkMenuValue) {
+            shrinkMenuClicked(shrinkMenuValue);
+        }
+    }
+    
+    const setShrinkMenu = () => {
+        let shrinkMenuValue = shrinkMenu ? false : true;
+        if (shrinkMenuValue) localStorage.setItem("shrinkMenu", "true");
+        else localStorage.removeItem("shrinkMenu");
+        shrinkMenuClicked(shrinkMenuValue);
+    }
+    
+    
     return (
-        <main className="main overflow-hidden">
-            <div className="side-bar flex flex-col h-full">
-                <div style={{borderBottom: "solid 0.5px var(--border)"}} className="flex-none flex items-center h-16 px-3 w-full">
+        <main className="main flex overflow-hidden">
+            <div className={`${shrinkMenu ? "shrunken-side-bar" : ""} side-bar flex flex-col h-full`}>
+                <div style={{borderBottom: "solid 0.5px var(--border)"}} className="flex-none flex items-center h-16 px-3 w-full shrink-center">
                     <div className="logo-container rounded w-8 h-8 flex items-center justify-center">
                         <img src="/disc-light.png" alt="logo" width={20} />
                     </div>
-                    <div className="ml-3">
+                    <div className="ml-3 shrink-hide">
                         <h1 className="text-sm font-medium">Disc Dashboard</h1>
                         <p style={{color: "var(--gray)"}} className="font-thin text-xs">Home</p>
                     </div>
                 </div>
                 <div className="flex-grow">
-
-                </div>
-                <div style={{borderTop: "solid 0.5px var(--border)"}} className="flex-none flex items-center h-16">
                     
                 </div>
+                <div style={{borderTop: "solid 0.5px var(--border)"}} className="flex-none flex items-center h-16">
+                    <button onClick={setShrinkMenu} className="p-0 w-6 h-6 flex items-center justify-center">
+                        <FontAwesomeIcon className="w-3 h-2.5" style={{color: "var(--dark-gray)"}} icon={shrinkMenu ? faArrowRight : faArrowLeft}/>
+                    </button>
+                </div>
+            </div>
+            <div className="flex-1">
+                {children}
             </div>
         </main>
+    )
+}
+
+export function DashboardTitle({title, description} : {title: string, description: string}) {
+    return (
+        <div className="px-2">
+            <h1 className="font-bold text-3xl">{title}</h1>
+        </div>
     )
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { authValidator } from "../components/authValidator";
-import DashboardTemplate from "../components/dashboard";
+import DashboardTemplate, { DashboardTitle } from "../components/dashboard";
 import { redirect } from "next/navigation";
 
 
@@ -7,6 +7,10 @@ export default async function Dashboard() {
     let redirectUrl = await authValidator(true);
     if (typeof redirectUrl == "string") redirect(redirectUrl);
     return (
-        <DashboardTemplate></DashboardTemplate>
+        <DashboardTemplate>
+            <div className="w-full h-full p-14">
+                <DashboardTitle title="Trending" description=""/>
+            </div>
+        </DashboardTemplate>
     )
 }
