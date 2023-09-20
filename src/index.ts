2d5d4b81883e8a54ccbc37338cb0ac3347776af2
Amitoj Singh Saini <gr8amu@gmail.com>
2023-09-20 01:12:01 -0400

    Added tailwind css, started creating login/signup page

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import { AuthReq, allowUsers, userValidationMiddleware } from "./middleware/authValidator";
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
-
-
+import path from "path";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+app.use("/public", express.static("src/static"));
 app.set("view engine", "pug");
+app.set("views", path.join("src", "views"));
 app.use(userValidationMiddleware)
 
-app.get("/", allowUsers((req: AuthReq, res: Response) => {
-    res.send("Hello you are logged out")
-}, false))
+// loggedout routes
+app.get("/", allowUsers((req: AuthReq, res: Response) => res.render("loggedout/index.pug"), false))
 
 app.listen(port, () => {
     console.log(`Disc Server is running at http://localhost:${port}`)
