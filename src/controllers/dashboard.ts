d69a8b55f0b2a604dac31b332edaa504b2c8a99d
Amitoj Singh Saini <gr8amu@gmail.com>
2023-09-29 00:36:29 -0400

    Creating dashboard ui

--- src/controllers/dashboard.ts
+++ src/controllers/dashboard.ts
@@ -2,6 +2,5 @@ import { AuthReq } from "../middleware/authValidator";
 import express, { Response } from "express";
 
 export const dashboard = (req: AuthReq, res: Response) => {
-    console.log(req.user);
-    res.send("hello world, you are logged in !!!!");
+    res.render("loggedin/index.pug")
 }
